e9d432fb89e4fb06ff065c8d7fbdcd9b0b4be83e
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2025-08-22 12:13:21 +0300

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50">
+      <div className="flex items-center justify-between bg-black text-white p-4">
+      </div>
+    </header>
+  );
+};
